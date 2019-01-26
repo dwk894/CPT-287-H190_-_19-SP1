fb1ad652442df6d9e6d823ad3e9ba8c411b24a17
Dayu Wang <dwk893@gmail.com>
2019-01-25 18:28:31 -0600

    Node

---
+++ Lecture_2/src/data_structures/Node.java
@@ -0,0 +1,11 @@
+/** Created by Dayu Wang (dwang@stchas.edu) on 01-25-19. */
+
+/** Last updated by Dayu Wang (dwang@stchas.edu) on 01-25-19. */
+
+package data_structures;
+
+public class Node<T> {
+	public T data;
+	public Node<T> next;
+	public Node(T theData) { data = theData; }
+}
